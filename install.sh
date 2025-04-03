6a8e20a13826679fd025f8b12cb5c9e4b9021def
Bartwel27 <81015407+Bartwel27@users.noreply.github.com>
2025-04-03 12:11:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/sys/bin/bash
 
 clear
+seconds=2
+echo "installing cPanel..."
+echo "waiting for ${seconds} to start installation..."
+sleep ${seconds}
+
+if command -v curl wget -q;then
+  echo "packages already installed"
+else
+ apt install curl wget
+ 
