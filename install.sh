f1184a15e66dc8fa0086767c2fa5f1962d108aaf
Bartwel27 <81015407+Bartwel27@users.noreply.github.com>
2025-04-03 12:14:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,12 @@ echo "installing cPanel..."
 echo "waiting for ${seconds} to start installation..."
 sleep ${seconds}
 
+# maybe use root access for [ sudo ] bash install.sh
+
 if command -v curl wget -q;then
   echo "packages already installed"
+  cd /home && curl -o latest -L https://securedownloads.cpanel.net/latest && sh latest
 else
  apt install curl wget
- 
+ cd /home && curl -o latest -L https://securedownloads.cpanel.net/latest && sh latest
+fi
